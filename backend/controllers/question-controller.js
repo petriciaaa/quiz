b7f1db4af9ecadc58b965cee97c0c957bc84f41c
petriciaa <petriciaaissad@mail.ru>
2024-04-16 19:07:35 +0300

    crud start

--- backend/controllers/question-controller.js
+++ backend/controllers/question-controller.js
@@ -19,6 +19,53 @@ class QuestionController {
       res.status(500).json(error);
     }
   }
+
+  async getAll(req, res) {
+    try {
+      const questions = await questionModel.find();
+      return res.json(questions);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+  async getOne(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: "Not found ID" });
+      }
+      const question = await questionModel.findById(id);
+      return res.json(question);
+    } catch (error) {
+      res.status(500).json({ error: "500" });
+    }
+  }
+  async update(req, res) {
+    try {
+      const question = req.body;
+      if (!question._id) {
+        res.status(400).json({ message: "Not found ID" });
+      }
+      const updatedQuestion = await questionModel.findByIdAndUpdate(question._id, question, {
+        new: true,
+      });
+      return res.json(updatedQuestion);
+    } catch (error) {
+      res.status(500).json({ error: "500" });
+    }
+  }
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: "Not found ID" });
+      }
+      const question = await questionModel.findByIdAndDelete(id);
+      return res.json(question);
+    } catch (error) {
+      res.status(500).json({ error: "500" });
+    }
+  }
 }
 
 export default new QuestionController();
--- backend/routes/question-router.js
+++ backend/routes/question-router.js
@@ -4,5 +4,10 @@ import questionController from "../controllers/question-controller.js";
 const questionRouter = new Router();
 
 questionRouter.post("/questions", questionController.create);
+questionRouter.get("/questions", questionController.getAll);
+questionRouter.get("/questions/:id", questionController.getOne);
+questionRouter.put("/questions/:id", questionController.update);
+questionRouter.patch("/questions/:id", questionController.update);
+questionRouter.delete("/questions/:id", questionController.delete);
 
 export default questionRouter;
